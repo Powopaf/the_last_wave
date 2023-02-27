2d55016c547bb7dee35a3ec39465a9de529e55c7
Powopaf <123662077+Powopaf@users.noreply.github.com>
2023-02-27 18:50:55 +0100

    start creating playermovement (#17)
    mouvement du joueur a réparer ne marche pas du tout

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -13,8 +13,10 @@ namespace Players
         private (string,int)[] _ressource_inv;
         private string _name;
         private  int _heal;
-        private int _speed;
-        
+        public float _speed;
+        public Rigidbody2D rb;
+        private Vector3 velo = Vector3.zero;
+
         public Player(int health = 1, int damage = 1,
             int speed = 1, int heal = 1, string name = "")
         {
@@ -27,11 +29,25 @@ namespace Players
             _item_inv = new List<Item>();
             _ressource_inv = new[] { ("wood", 0), ("stone", 0), ("iron", 0) };
         }
-        
-        private void CoordUpdate(int x, int y)
+
+        private void FixedUpdate()
         {
-            _coordinate.X += x;
-            _coordinate.Y += y;
+            float horizontalMovement = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+            float verticalMOvement = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+
+
+        }
+
+        private void MovePLayer(float horizontalMovement, float verticalMovement)
+        {
+            Vector3 targetVelocityY = new Vector2(horizontalMovement, rb.velocity.y);
+            //rb.velocity = Vector3.SmoothDamp(rb.velocity, 
+            //    targetVelocityY, ref targetVelocityY, .05f);
+            Vector3 targetVelocityX = new Vector2(verticalMovement, rb.velocity.x);
+            /*rb.velocity = Vector3.SmoothDamp(rb.velocity,
+                targetVelocityX, ref targetVelocityX, .05f);
+        */
+            rb.AddForce(targetVelocityX);
         }
         
         private void Looting(Item[] loot)

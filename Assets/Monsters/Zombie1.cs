15f3c69ef96d31db92d00a6b9ee039742124e6e4
Atkinson Matteo <73189720+R01DESJELLOS@users.noreply.github.com>
2023-03-01 16:09:45 +0100

    Movement zombie 1 (#20)
    * Zombie Basic Movement 1
    
    ça marche pas maisau calme bientot
    
    * Basic Focus Zombie finish
    
    le focus  basique du joueur par un zombie est finis

--- Assets/Monsters/Zombie1.cs
+++ Assets/Monsters/Zombie1.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 
 public class Zombie1 : Zombie
 {
-    public Zombie1() :
-        base("Zombie1", 
-            new []{"Player", "Core"},
-            100, 20, 30) {}
+    public Zombie1() : 
+        base("Zombie1", new []{"Player", "Core"}, 
+        100, 20, 30) {}
 
-    private static void Movement()
+    protected override void Start()
     {
-        throw new NotImplementedException();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    protected  override void ZombieMovement(Vector2 direction)
+    {
+        rb.MovePosition((Vector2) transform.position +direction * (_speed * Time.deltaTime));
+    }
+
+    protected override void Update()
+    {
+        Vector3 direction = Player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    protected override void FixedUpdate()
+    {
+        ZombieMovement(movement);
     }
 }

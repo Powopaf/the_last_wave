41524ae3c3e38562f4e8b49b9d564556b5e3c244
Powopaf <123662077+Powopaf@users.noreply.github.com>
2023-03-03 15:43:24 +0100

    finish batiment (#24)

--- Assets/World/MapDefinition.cs
+++ Assets/World/MapDefinition.cs
@@ -6,10 +6,10 @@
 
     public MapDefinition()
     {
-        Map = new int[,]
+        Map = new [,]
         {
             { 1, 1, 1, 1 },
-            { 1, 0, 0, 1 },
+            { 1, 0, 3, 1 },
             { 1, 1, 1, 1 },
         };
     }

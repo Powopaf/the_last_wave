84e498978c0b29a72f5644074dcc59946ebdab26
Atkinson Matteo <73189720+R01DESJELLOS@users.noreply.github.com>
2023-03-04 14:32:32 +0100

    Zombie 2 movement (#26)
    les mouvements du zombie 2 sont fini,

--- Assets/Monsters/Zombie2.cs
+++ Assets/Monsters/Zombie2.cs
@@ -5,29 +5,40 @@ namespace Monsters
 {
     public class Zombie2 : Zombie
     {
-        public Zombie2() :
+        public float playerdistance;
+
+        public Zombie2(float distance=3) :
             base("Zombie2",
                 new[] { "Core" },
                 50, 15, 100)
-        {}
+        {
+            playerdistance = distance;
+        }
 
         protected override void Start()
         {
-            throw new NotImplementedException();
+            rb = GetComponent<Rigidbody2D>();
         }
 
         protected override void Update()
         {
-            throw new NotImplementedException();
+            Vector3 direction = Player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+            rb.rotation = angle;
+            direction.Normalize();
+            movement = direction;
         }
 
         protected override void FixedUpdate()
         {
-            throw new NotImplementedException();
+            ZombieMovement(movement);
         }
         protected override void ZombieMovement(Vector2 direction)
         {
-            throw new NotImplementedException();
+            if ((Player.position-transform.position).magnitude > playerdistance)
+            {
+                rb.MovePosition((Vector2)transform.position + direction * (_speed * Time.deltaTime));
+            }
         }
 
         protected override void OnCollisionEnter2D(Collision2D col)

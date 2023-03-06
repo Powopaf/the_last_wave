3627ef1e456655f1d0beb2ecd8d37045d82405fd
Atkinson Matteo <73189720+R01DESJELLOS@users.noreply.github.com>
2023-03-06 13:32:17 +0100

    Zombie 2 attack
    attack finish

--- Assets/Monsters/Zombie2projectile.cs
+++ Assets/Monsters/Zombie2projectile.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Monsters;
 using UnityEngine;
 
-public class Zombie2projectile : MonoBehaviour
+public class Zombie2projectile : Zombie2
 {
     public float bulletspeed;
 
@@ -12,14 +12,19 @@ public class Zombie2projectile : MonoBehaviour
     {
         bulletspeed = speed;
     }
-    void  Update()
+    new void  Update()
      {
          transform.position += transform.right * (Time.deltaTime * bulletspeed);
          
      }
 
-     void OnCollisionEnter2D(Collision2D col)
+     new void OnCollisionEnter2D(Collision2D col)
      {
+         if (col.transform.CompareTag("Player"))
+         {
+             Players.Survivor survivor = Playertarget.transform.GetComponent<Players.Survivor>(); //Zombie Attack
+             survivor.ZombieDamageOnPlayer(_damage); // 
+         }
          Destroy(gameObject);
      }
 }

1b16f0e021f3d9e471ce12a695aa04f22cfe760c
Powopaf <paf.fournier@gmail.com>
2023-03-06 13:53:33 +0100

    fix speed camera

--- Assets/Scenes/Cams/CameraFollow.cs
+++ Assets/Scenes/Cams/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float CamFollowSpeed = 2f;
+    private float CamFollowSpeed = 30f;
     public Transform target;
     
     // Update is called once per frame

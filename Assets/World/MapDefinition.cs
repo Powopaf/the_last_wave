4d7cb4522c04b42fe33160a9febac55c5a9dbe82
Powopaf <paf.fournier@gmail.com>
2023-03-08 11:24:23 +0100

    add texture map

--- Assets/World/MapDefinition.cs
+++ Assets/World/MapDefinition.cs
@@ -1,4 +1,6 @@
-﻿public class MapDefinition
+﻿using Random = System.Random;
+
+public class MapDefinition
 {
     public int[,] Map { get; }
     public int Height => Map.GetLength(0);
@@ -6,11 +8,22 @@
 
     public MapDefinition()
     {
-        Map = new [,]
+        Random rd = new Random(0);
+        Map = new int[100, 100];
+        for (int i = 0; i < Width; i++)
         {
-            { 1, 1, 1, 1 },
-            { 1, 0, 3, 1 },
-            { 1, 1, 1, 1 },
-        };
+            Map[0,i] = 13;
+            Map[Width-1, i] = 13;
+            Map[i, 0] = 13;
+            Map[i, Width - 1] = 13;
+        }
+
+        for (int i = 1; i < Height - 1; i++)
+        {
+            for (int j = 1; j < Width - 1; j++)
+            {
+                Map[i, j] = rd.Next(0,3);
+            }
+        }
     }
 }

dc23e639a9d3316f61ea0b6942ae851aa503678f
Atkinson Matteo <73189720+R01DESJELLOS@users.noreply.github.com>
2023-03-08 14:10:16 +0100

    Animation modifier

--- Assets/Scenes/Cams/CameraFollow.cs
+++ Assets/Scenes/Cams/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private float CamFollowSpeed = 30f;
+    private float CamFollowSpeed = 2100f;
     public Transform target;
     
     // Update is called once per frame

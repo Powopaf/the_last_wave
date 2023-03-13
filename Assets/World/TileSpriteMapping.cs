93c3b96c7ee62704ca9e14bfaa9767f38a1518c0
Powopaf <123662077+Powopaf@users.noreply.github.com>
2023-03-13 10:09:22 +0100

    Finition map (#69)
    * init
    
    * finish map implementation

--- Assets/World/TileSpriteMapping.cs
+++ Assets/World/TileSpriteMapping.cs
@@ -5,10 +5,9 @@ namespace World
 {
     public class TileSpriteMapping : MonoBehaviour
     {
-        public Tile[] tiles;
         public IDictionary<TileType, int> Sprites { get; } = new Dictionary<TileType, int>
         {
-            { TileType.GroundBlack, 0 },
+            { TileType.GroundWhite, 0 },
             { TileType.GroundDirt, 1 },
             { TileType.GroundGrassLight, 2 }
         };

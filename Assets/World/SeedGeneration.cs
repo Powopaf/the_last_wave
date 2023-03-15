115af0125618c1803a44f7711e445152cd6fe59d
Powopaf <paf.fournier@gmail.com>
2023-03-15 20:36:14 +0100

    finish distance

---
+++ Assets/World/SeedGeneration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace World
+{
+    public class SeedGeneration : MapDefinition
+    {
+        private List<(EnumTile, double, int, int)> Seeds { get; } = new();
+        private readonly Random _rd = new(0);
+        private TileSprite _tileSprite;
+        
+        private double NextDouble()
+        {
+            double maxValue = 2;
+            return _rd.NextDouble() * maxValue;
+        }
+
+        private void GenerateSeeds(int n = 1)
+        {
+            List<(int, int)> postaken = new List<(int, int)>();
+            for (int i = 0; i < n; i++)
+            {
+                int x = _rd.Next(0, Height);
+                int y = _rd.Next(0, Width);
+                if (!postaken.Contains((x,y)))
+                {
+                    postaken.Add((x,y));
+                    EnumTile tile = _tileSprite.EnumTiles[_rd.Next(0, _tileSprite.Sprite.Count)];
+                    double intensity = NextDouble();
+                    Seeds.Add((tile, intensity, x, y));
+                }
+            }
+        }
+
+        private (EnumTile, double, int, int) Distance(int i, int j)
+        {
+            double dismin = Math.Sqrt((i - Seeds[0].Item3) * (i - Seeds[0].Item3)
+                                      + (j - Seeds[0].Item4) * (j - Seeds[0].Item4));
+            (EnumTile, double, int, int) seed = Seeds[0];
+            foreach (var s in Seeds)
+            {
+                int x = s.Item3;
+                int y = s.Item4;
+                double dis = Math.Sqrt((i - x) * (i - x) + (j - y) * (j - y));
+                if (dis < dismin)
+                {
+                    dismin = dis;
+                    seed = s;
+                }
+            }
+            return seed;
+        }
+    }
+}

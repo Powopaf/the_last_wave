9561d7485ec9a6bef782c5a623bf3af4caa66575
Powopaf <paf.fournier@gmail.com>
2023-03-17 21:26:10 +0100

    fix some bug

--- Assets/World/MapDefinition.cs
+++ Assets/World/MapDefinition.cs
@@ -10,23 +10,18 @@ namespace World
 
         public MapDefinition()
         {
-            //uint count = 0;
-            Map = new TileDefinition[20, 25];
+            Map = new TileDefinition[200, 200];
             for (int i = 0; i < Width; i++)
             {
                 Map[0, i] = new TileDefinition(EnumTile.GroundWhite);
                 Map[Height - 1, i] = new TileDefinition(EnumTile.GroundWhite);
-                //count += 2;
             }
             for (int j = 0; j < Height; j++)
             {
                 Map[j, 0] = new TileDefinition(EnumTile.GroundWhite);
                 Map[j, Width - 1] = new TileDefinition(EnumTile.GroundWhite);
-                //count += 2;
             }
-            //DefaultMap();
-            //Debug.Log(count);
-            SeedMap(5);
+            SeedMap(100);
         }
 
         private void DefaultMap()

4fcb985cc02df1580c94f4bd073899be2169bf8f
Powopaf <paf.fournier@gmail.com>
2023-03-19 16:52:43 +0100

    add grass side

--- Assets/World/EnumTile.cs
+++ Assets/World/EnumTile.cs
@@ -7,6 +7,10 @@
         Grass1,
         Sand1,
         Snow1,
-        Water1
+        Water1,
+        GrassSideLeft,
+        GrassSideRight,
+        GrassSideTop,
+        GrassSideBot
     }
 }

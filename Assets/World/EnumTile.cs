3c4049d9aa07e8592bce88b54f57eb0d2e846536
Powopaf <paf.fournier@gmail.com>
2023-04-09 23:49:49 +0200

    add more Grass

--- Assets/World/EnumTile.cs
+++ Assets/World/EnumTile.cs
@@ -26,7 +26,21 @@
         /// <summary>
         /// Grass Tile
         /// </summary>
+        DefaultGrass1,
+        DefaultGrass2,
+        DefaultGrass3,
+        DefaultGrass4,
+        /// <value>
+        ///  1,2,3,4 go on top 5,6,7,8 got bellow 1234
+        /// </value>
         Grass1,
+        Grass2,
+        Grass3,
+        Grass4,
+        Grass5,
+        Grass6,
+        Grass7,
+        Grass8,
         GrassSideLeft,
         GrassSideRight,
         GrassSideTop,

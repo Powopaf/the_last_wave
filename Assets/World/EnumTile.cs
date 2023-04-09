1c8b19f03cbe66d9518665fb45ed4551bcf2a47b
Powopaf <paf.fournier@gmail.com>
2023-04-09 23:29:30 +0200

    rebuild Ressoures folder archi

--- Assets/World/EnumTile.cs
+++ Assets/World/EnumTile.cs
@@ -2,13 +2,31 @@
 {
     public enum EnumTile
     {
+        /// <summary>
+        ///  WallBorder and Tile None
+        /// </summary>
         NoTile = 0,
         WallBorderMap,
+        /// <summary>
+        /// Dirt Tile
+        /// </summary>
         Dirt1,
-        Grass1,
+        /// <summary>
+        /// Sand Tile
+        /// </summary>
         Sand1,
+        /// <summary>
+        /// Snow Tile
+        /// </summary>
         Snow1,
+        /// <summary>
+        /// Water tile
+        /// </summary>
         Water1,
+        /// <summary>
+        /// Grass Tile
+        /// </summary>
+        Grass1,
         GrassSideLeft,
         GrassSideRight,
         GrassSideTop,

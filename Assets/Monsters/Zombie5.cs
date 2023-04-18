42b98c42f73e6c23fef704efdd965bfb05b95190
Atkinson Matteo <73189720+R01DESJELLOS@users.noreply.github.com>
2023-04-18 14:08:37 +0200

    Basics movement for all Zombie finish

--- Assets/Monsters/Zombie5.cs
+++ Assets/Monsters/Zombie5.cs
@@ -1,61 +1,57 @@
-using System;
-using System.Collections.Generic;
+using System.Collections;
 using Pathfinding;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Monsters
 {
-    public class Zombie4 : Zombie
+    public class Zombie5 : Zombie
     {
         private static readonly int X = Animator.StringToHash("X");
         private static readonly int Y = Animator.StringToHash("Y");
 
-        public Zombie4() :
-            base("Zombie4",
-                new[] { "Player", "Core" },
-                100, 85, 70)
-        {
-        }
-
+        public Zombie5() : 
+            base("Zombie1", new []{"Assassin","Farmer","Survivor","Worker"}, 
+                10000, 100, 30) {}  
+    
         protected override void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
-            AI = GetComponent<AIPath>();
+            AI=GetComponent<AIPath>();
 
         }
-
         protected override void Start()
         {
+           
         }
-
+       
 
         protected override void ZombieMovement(Vector2 direction)
         {
         }
-
-
+        
+       
         protected override void Update()
         {
             Movement = AI.desiredVelocity;
             animator.SetFloat(X, Movement.x);
             animator.SetFloat(Y, Movement.y);
         }
-
         protected override void FixedUpdate()
         {
         }
 
-
+       
         protected override void OnCollisionEnter2D(Collision2D col)
         {
-            if (((IList<string>)_target).Contains(col.transform.tag)) //Need to change  the tag
+            if ( ((IList)_target).Contains(col.transform.tag))   //Need to change  the tag
             {
                 Players.Survivor survivor = Playertarget.transform.GetComponent<Players.Survivor>(); //Zombie Attack
                 survivor.ZombieDamageOnPlayer(Damage); // Zombie Attack
             }
-
+        
         }
+    
     }
 }
+

4e08415378c619a0074b0292ec758348e8f1492f
Powopaf <paf.fournier@gmail.com>
2023-05-26 22:19:42 +0200

    finish turret

--- Assets/Players/Turret.cs
+++ Assets/Players/Turret.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Monsters;
 using Photon.Pun;
 using UnityEngine;
@@ -8,26 +7,23 @@ namespace Players {
     public class Turret : MonoBehaviour
     {
         private readonly string[] _target =  { "Survivor" };
-
-        void Start()
-        {
-            
-        }
-
+        public ParticleSystem particle;
+        
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (_target.Contains(col.transform.tag))
             {
-                var p = PhotonNetwork.Instantiate("Fire", col.transform.position, Quaternion.identity);
-                p.GetComponent<ParticleSystem>().Play();
-                if (col.transform.CompareTag("Survivor"))
+                particle.transform.position = col.transform.position;
+                particle.Play();
+                if (col.transform.CompareTag("Zombie"))
                 {
-                    Farmer zombie1 = col.transform.GetComponent<Farmer>(); // chenger en fonction du métier
-                    if (false)
+                    Zombie1 zombie1 = col.transform.GetComponent<Zombie1>(); // changer en fonction du zombie
+                    if (zombie1.ZombieTakeDamage(5))
                     {
                         PhotonNetwork.Destroy(col.gameObject);
                     }
                 }
+                particle.Stop();
             }
         }
     } 

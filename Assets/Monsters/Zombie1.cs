943c9c3ded1162f917d8bc2cbbc04dc79ff0fd39
Powopaf <123662077+Powopaf@users.noreply.github.com>
2023-05-27 08:58:08 +0200

    Fix damage (#160)
    * init
    
    * fix damage zombie

--- Assets/Monsters/Zombie1.cs
+++ Assets/Monsters/Zombie1.cs
@@ -46,7 +46,7 @@ namespace Monsters
         }
 
        
-        protected override void OnCollisionEnter2D(Collision2D col)
+        protected void OnCollisionStay2D(Collision2D col)
         {
             
             if (Target.Contains(col.transform.tag))   //Need to change  the tag
